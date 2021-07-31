830f539fbc4b64204ff575beb29b13c6da35953b
jotape00 <jpbasaglia@outlook.com>
2021-07-30 22:36:47 -0300

    Projeto Spring - Organização de loja de games

---
+++ lojagames/src/main/java/br/org/generation/lojagames/model/Categoria.java
@@ -0,0 +1,54 @@
+package br.org.generation.lojagames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id //id é uma chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // função de auto_increment
+	private long id;
+	
+	@NotNull(message = "O atributo tipo não pode ser nulo.") // o campo não pode ser nulo
+	private String tipo;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto; // cria uma lista de postagens
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+}
